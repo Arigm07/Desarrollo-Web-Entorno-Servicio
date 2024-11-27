4a23e2a89b0deec1226a806a73db545bb2f85388
Ariadna <agarciam275@educarex.es>
2024-11-27 10:54:07 +0100

    nuevo

---
+++ TEMAS_ ARI/U3/skills/Modelo.php
@@ -0,0 +1,24 @@
+<?php
+require_once "Modalidad.php";
+require_once "Alumno.php";
+require_once "Correcion.php";
+require_once "Controlador.php";
+
+class Modelo{
+
+
+    private $conexion = null;
+    
+
+    function __construct(){
+        
+    }
+
+    function obtenerModalidades(){
+
+    }
+
+}
+
+
+?>
