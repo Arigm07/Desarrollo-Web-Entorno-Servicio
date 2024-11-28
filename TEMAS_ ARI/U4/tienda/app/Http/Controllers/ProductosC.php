ecc4e74cf895456593d9ea961fa7e9b5740196c3
Ariadna <agarciam275@educarex.es>
2024-11-28 10:01:51 +0100

    d

--- TEMAS_ ARI/U4/tienda/app/Http/Controllers/ProductosC.php
+++ TEMAS_ ARI/U4/tienda/app/Http/Controllers/ProductosC.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -14,6 +15,10 @@ class ProductosC extends Controller
     }
 
     function verProductos(){
-        return view('productos/verProductos');
+        //Recuperamos los productos (equivale a select * from productos)
+            //y los devuelve en un array
+        $productos = Producto::all();
+        return view('productos/verProductos',compact('productos'));
+            //return view('productos/verProductos',['productos'=> productos]);
     }
 }
